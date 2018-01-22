f34238a2a2aa2c1e276114ceafa6b15faa4a96e0
Zushisan <74838@supinfo.com>
2018-01-22 16:01:19 +0000

    lower case ?

--- callbacks/print-lowercase.js
+++ callbacks/print-lowercase.js
@@ -1,1 +1,13 @@
 var getHTML = require('./http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/lowercase.html'
+};
+
+function printHTML (html) {
+  console.log(html.toLowerCase());
+}
+
+
+getHTML(requestOptions, printHTML);
