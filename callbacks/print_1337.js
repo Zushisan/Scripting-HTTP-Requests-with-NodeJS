0dade2788a9009efc7fcd9bc439111ce30ccc073
Zushisan <74838@supinfo.com>
2018-01-22 16:29:49 +0000

    l33t v1

--- callbacks/print_1337.js
+++ callbacks/print_1337.js
@@ -1,1 +1,35 @@
 var getHTML = require('./http-functions');
+
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/1337.html'
+};
+
+function printHTML (html) {
+
+
+  // function L337(str) {
+  //   return str.replace(/[a-z]/g,function f(a){
+  //     return "4BCD3F6H1JKLMN0PQR57"[parseInt(a, 36)-10] || a.replace(/[a-t]/gi,f)
+  //   });
+  // }
+
+  function LeetTest(str){
+    return str.replace(/a/g, "4").replace(/e/g, "3").replace(/er/g, "0r").replace(/you/g, "j00");
+  }
+
+  console.log(LeetTest(html));
+
+
+}
+
+
+getHTML(requestOptions, printHTML);
+
+// function LeetTest(str){
+//   return str.replace(/[a]/g: "4", /[e]/g: "3", /[l]/g :"1")
+// }
+
+
+// a : '4', e : '3', l : '1', o : '0', s : '5', t : '7', 'ck' : 'x', 'er' : '0r', 'you' : 'j00'
